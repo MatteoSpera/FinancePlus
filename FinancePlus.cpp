bd5965457b77cf575e5977914d5dfbb6e1fbe35a
MatteoSpera <matteo.spera0301@gmail.com>
2023-08-21 22:24:55 -0300

    Criada Estrutura Inicial do Programa

---
+++ FinancePlus.cpp
@@ -0,0 +1,36 @@
+struct Data {
+    int dia;
+    int mes;
+    int ano;
+};
+//TODO - criar métodos para data
+
+struct Pessoa
+{
+    int id;
+    char nome[40];
+};
+
+struct Categoria_Gasto
+{
+    // Categoria do Gasto realizado, como entretenimento, comida, locomoção, médico, etc
+    int id;
+    char descricao[40]; 
+};
+
+struct Banco
+{
+    int id;
+    char descricao[40];
+};
+
+struct Transacao
+{
+    int id;
+    int idCategoria;
+    int idConta;
+    Data data;
+    float valor;
+    char tipo; //Débito ou Crédito
+};
+
